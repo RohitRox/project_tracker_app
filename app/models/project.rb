50df5f4c5bc3d0624dfa34b7b2651be30d601042
Rohit Joshi <link2j.roy@gmail.com>
2011-12-15 17:32:22 +0545

    task add spec and project access authorized

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :tasks
-  validates :deadline, :presence => true 
-  validates :name, :presence => true ,:length => {:minimum => 6}
+  validates :deadline, :presence => true , :format => { :with => /^(([1][012])||[0][1-9])\/(([0][1-9])||([12][0-9])||([3][0-2]))\/([2][0-9][1-9][0-9])$/}
+  validates :name, :presence => true ,:length => {:minimum => 6},:format => {:with =>/^(\w|\s|[-]|['])*$/i}
   validates :description, :presence => true 
 end
+
